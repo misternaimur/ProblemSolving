54a8c829b34de4e222792fd4cf8446159410363c
Md Naimur Rahman <jawadnaimur@gmail.com>
2025-05-16 12:21:19 +0600

    first commit

---
+++ problem1003.c
@@ -0,0 +1,12 @@
+// Beecrowd 
+#include<stdio.h>
+
+int main()
+{
+    int A, B, Sum;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    Sum = A + B;
+    printf("SOMA = %d\n", Sum);
+    return 0;
+}
---
+++ problem1004.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+     int a , b ,PORD; 
+     scanf("%d",&a);
+     scanf("%d",&b);
+     printf("PROD = %d\n", PORD=a*b);
+     return 0; 
+
+}
