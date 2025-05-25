930a0b9a194a3ac16d7b93e1088bbfd584ec53e2
Md Naimur Rahman <jawadnaimur@gmail.com>
2025-05-25 10:35:59 +0600

    problem Solving

--- Problem1009.c
+++ Problem1009.c
@@ -8,18 +8,22 @@
 
 */
 
-#include<stdio.h>
-int main()
-{
-     char name[10];
-     float fixed_salary, persentis_ammount , total_Salary;
-     scanf("%s\n",&name);
-     scanf("%f\n",&fixed_salary);
-     scanf("%f",&persentis_ammount);
-     
-     persentis_ammount =((float)15/100)*persentis_ammount;
-     total_Salary = fixed_salary+persentis_ammount;
-     printf("Total = R$ %.2f\n",total_Salary);
-     return 0 ; 
 
+#include <stdio.h>
+
+int main() {
+    char name[100];
+    double fixed_salary, sales_amount, total_salary;
+
+    scanf("%s", name);
+    scanf("%lf", &fixed_salary);
+    scanf("%lf", &sales_amount);
+
+    total_salary = fixed_salary + (0.15 * sales_amount);
+
+    printf("TOTAL = R$ %.2lf\n", total_salary);
+
+    return 0;
 }
+
+
